c40d01e824eb7dc72c14d5bfe7c30e7c91d17fa4
NiklasvG <niklasblk1997@googlemail.com>
2023-08-12 17:23:17 +0200

    Started new Nextjs project. Added Next-Auth and Navbar with theming

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+	return (
+		<main className="bg-gray-950 min-h-screen text-gray-100 grid place-items-center">
+			<Button>Click me</Button>
+		</main>
+	)
+}
